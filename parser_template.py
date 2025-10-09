554bb514dcb19a3e9f5ccdfec512fd8e109c742c
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-10-09 14:00:08 +0200

    format

--- parser_template.py
+++ parser_template.py
@@ -19,26 +19,10 @@ class Parser:
 
     def predefined_symbols(self):
         return {
-            'R0': 0,
-            'R1': 1,
-            'R2': 2,
-	        'R3': 3,
-	        'R4': 4,
-            'R5': 5,
-            'R6': 6,
-	        'R7': 7,
-            'R8': 8,
-            'R9': 9,
-            'R10': 10,
-	        'R11': 11,
-	        'R12': 12,
-            'R13': 13,
-            'R14': 14,
-	        'R15': 15,
-	        'LOOP' : 10,
-	        'END' : 23
-	        'SCREEN' : 16384,
-	        'KBD' : 24576,
+            'R0': 0, 'R1': 1, 'R2': 2, 'R3': 3, 'R4': 4, 'R5': 5,\
+			'R6': 6, 'R7': 7, 'R8': 8, 'R9': 9, 'R10': 10, 'R11': 11,\
+	        'R12': 12, 'R13': 13, 'R14': 14, 'R15': 15, 'LOOP' : 10, 'END' : 23\
+	        'SCREEN' : 16384, 'KBD' : 24576,
             }
                         
     def parse(self)->None:
@@ -50,7 +34,7 @@ class Parser:
         self.replace_symbols()
 
     def clean_line(self, line:str)->str|None:
-        ''''
+        '''
         Bereinigt eine Zeile, indem Leerzeichen und Kommentare entfernt werden.
         Gibt None zurück, wenn die Zeile leer oder ein Kommentar ist.
         '''
@@ -62,14 +46,14 @@ class Parser:
             return line
     
     def return_type(self, line:str)->str:
-        ''''
+        '''
         Bestimmt den Typ einer Anweisung.
         Gibt 'A' für A-Befehle, 'C' für C-Befehle und 'L' für Labels zurück.
         '''
         return None
 
     def add_labels(self)->None:
-        ''''
+        '''
         Erster Durchlauf: Fügt Labels zum Symboltabelle hinzu und speichert die
         Adressen der Anweisungen.
         '''
@@ -81,7 +65,7 @@ class Parser:
                 #dein Code hier
             
     def replace_symbols(self)->None:
-        ''''
+        '''
         Zweiter Durchlauf: Ersetzt Symbole durch Adressen.
         '''
         line_number = 0
