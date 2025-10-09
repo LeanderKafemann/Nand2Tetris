a131199ed9541e6d1b0318192bb7bcb9005833df
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-10-09 14:01:55 +0200

    Add docstring to predefined_symbols method
    Added docstring for predefined_symbols method.

--- parser_template.py
+++ parser_template.py
@@ -18,6 +18,9 @@ class Parser:
         #dein Code hier
 
     def predefined_symbols(self):
+		"""
+		Vordefinierte Symbole.
+		"""
         return {
             'R0': 0, 'R1': 1, 'R2': 2, 'R3': 3, 'R4': 4, 'R5': 5,\
 			'R6': 6, 'R7': 7, 'R8': 8, 'R9': 9, 'R10': 10, 'R11': 11,\
