0ab5bb7e4794c43e18ceea243545b7c2158e6b6f
leander@kafemann.berlin <leander@kafemann.berlin>
2025-10-16 17:37:11 +0200

    make folders compilable

--- 06/assembler_template.py
+++ 06/assembler_template.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import parser_template as p
 import instructions_template as i
 
@@ -6,29 +6,34 @@ print("--- Skript gestartet ---", flush=True)
 
 # 1. Prüfen, ob der Dateiname als Argument übergeben wurde
 if len(sys.argv) < 2:
-    filename = input("Dateipfad angeben: ")
+    filenames = [input("Dateipfad angeben: ")]
 else:# Der Dateiname ist das erste Argument nach dem Skriptnamen
-    filename = sys.argv[1]
+    if os.path.isdir(sys.arvv[1]):
+        dir_path = sys.argv[1]
+        filenames = [os.path.join(dir_path, f) for f in os.listdir(dir_path) if f.endswith('.asm')]
+    else:
+        filenames = [sys.argv[1]]
 
-# Datei öffnen und Fehler abfangen
-try:
-    with open(filename, 'r') as file:
+for filename in filenames:
+    # Datei öffnen und Fehler abfangen
+    try:
+        with open(filename, 'r') as file:
         
-        print(f"--- Parser for '{filename}' ---")
-        #2. Erzeuge ein Parser Objekt
-        parser = p.Parser(file)
-        #Ergebniss des Parsers ausgeben -> Print Methode in parser.py anpassen
-        #kann auch auskommentiert werden
-        parser.print()
+            print(f"--- Parser for '{filename}' ---")
+            #2. Erzeuge ein Parser Objekt
+            parser = p.Parser(file)
+            #Ergebniss des Parsers ausgeben -> Print Methode in parser.py anpassen
+            #kann auch auskommentiert werden
+            parser.print()
 
-        #3. Erzeuge ein Instructions Objekt
-        instructions = i.Instructions(parser)
-        #4. Schreibe den Maschinencode in eine .hack Datei
-        instructions.write(filename)
+            #3. Erzeuge ein Instructions Objekt
+            instructions = i.Instructions(parser)
+            #4. Schreibe den Maschinencode in eine .hack Datei
+            instructions.write(filename)
 
-except FileNotFoundError:
-    # 5. Fehlerbehandlung
-    print(f"Error: Die Datei '{filename}' wurde nicht gefunden.")
-    sys.exit(1)
+    except FileNotFoundError:
+        # 5. Fehlerbehandlung
+        print(f"Error: Die Datei '{filename}' wurde nicht gefunden.")
+        sys.exit(1)
 
-print("--- Verarbeitung abgeschlossen ---")
+    print("--- Verarbeitung abgeschlossen ---")
