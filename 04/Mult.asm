77a7c4083ea97037d62e3e50ba0c6a1cbf7497fc
NotGhostpro <matheokruger18@gmail.com>
2025-10-17 13:45:49 +0200

    Update Mult.asm

--- 04/Mult.asm
+++ 04/Mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
+@R2
+M=0
+
+@R0
+D=M
+@n
+M=D
+@R1
+D=M
+@m
+M=D
+
+(LOOP)
+@m
+D=M
+@END
+D;JEQ
+@n
+D=M
+@R2
+M=D+M
+@m
+M=M-1
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
