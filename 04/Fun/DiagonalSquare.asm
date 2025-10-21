748f2824fe07eacef248593506cfd8ac53e74005
NotGhostpro <matheokruger18@gmail.com>
2025-10-21 21:41:46 +0200

    Implement screen clearing functionality
    Added code to clear the screen before drawing.

--- 04/Fun/DiagonalSquare.asm
+++ 04/Fun/DiagonalSquare.asm
@@ -1,2 +1,28 @@
 //creates a 45 degrees rotated square
 //Outercircleradius in R0(1-128)
+
+//clear screen
+@8192
+D=A
+@clear
+M=D
+@SCREEN
+D=A
+@cleara
+M=D
+(CLEAR)
+@clear
+D=M
+@GO
+D;JEQ
+@cleara
+A=M
+M=0
+@cleara
+M=M+1
+@clear
+M=M-1
+@CLEAR
+0;JMP
+
+(GO)
